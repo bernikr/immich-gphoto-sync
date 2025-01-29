81076d2f899b6d55c7d0323cf1b201db9b93adde
bernikr <git@berni.kr>
2025-01-29 23:00:28 +0100

    refactor google api into its own context manager

--- google_photos.py
+++ google_photos.py
@@ -1,44 +1,62 @@
 import asyncio
 import re
+from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
 from pathlib import Path
 
-from playwright.async_api import Page
+from playwright.async_api import BrowserContext, Page, async_playwright
+
+from config import HEADLESS, USER_DATA_FOLDER
 
 
-async def get_photo_ids(page: Page, album_id: str, key: str) -> list[str]:
-    photos: dict[str, None] = {}
-    previous_height = 0
-    await page.goto(f"https://photos.google.com/share/{album_id}?key={key}")
+class GooglePhotosApi:
+    def __init__(self, browser: BrowserContext, page: Page) -> None:
+        self.browser = browser
+        self.page = page
 
-    scroll_offset = await page.evaluate("window.innerHeight") / 2
-    while True:
-        new_photos = await page.get_by_role("link").all()
-        new_photos = [await p.get_attribute("href") for p in new_photos]
-        photos |= {
-            p: None for p_link in new_photos if p_link is not None for p in re.findall(r"photo\/([^\/?]+)", p_link)
-        }
-        await page.evaluate(
-            f"document.querySelector('c-wiz[__is_owner=true]').scrollTo(0, {previous_height + scroll_offset})",
-        )
+    async def get_photo_ids(self, album_id: str, key: str) -> list[str]:
+        photos: dict[str, None] = {}
+        previous_height = 0
+        await self.page.goto(f"https://photos.google.com/share/{album_id}?key={key}")
+
+        scroll_offset = await self.page.evaluate("window.innerHeight") / 2
+        while True:
+            new_photos = await self.page.get_by_role("link").all()
+            new_photos = [await p.get_attribute("href") for p in new_photos]
+            photos |= {
+                p: None for p_link in new_photos if p_link is not None for p in re.findall(r"photo\/([^\/?]+)", p_link)
+            }
+            await self.page.evaluate(
+                f"document.querySelector('c-wiz[__is_owner=true]').scrollTo(0, {previous_height + scroll_offset})",
+            )
+            await asyncio.sleep(0.3)
+            current_height = await self.page.evaluate("document.querySelector('c-wiz[__is_owner=true]').scrollTop")
+            if current_height == previous_height:
+                return list(photos)
+            previous_height = current_height
+
+    async def load_album_meta(self, url: str) -> tuple[str, str, str]:
+        await self.page.goto(url)
+        await self.page.wait_for_url("https://photos.google.com/share/*")
+        galbum_id, gkey = re.findall(r"share\/([^\/?]+).*[?&]key=([^\/&]+)", self.page.url)[0]
+        title = (await self.page.title()).replace(" - Google Photos", "")
+        return galbum_id, gkey, title
+
+    async def download_photo(self, album_id: str, key: str, image_id: str) -> tuple[Path, str]:
+        await self.page.goto(f"https://photos.google.com/share/{album_id}/photo/{image_id}?key={key}")
         await asyncio.sleep(0.3)
-        current_height = await page.evaluate("document.querySelector('c-wiz[__is_owner=true]').scrollTop")
-        if current_height == previous_height:
-            return list(photos)
-        previous_height = current_height
+        async with self.page.expect_download() as download_info:
+            await self.page.keyboard.press("Shift+D")
+        download = await download_info.value
+        return await download.path(), download.suggested_filename
 
 
-async def load_album_meta(page: Page, url: str) -> tuple[str, str, str]:
-    await page.goto(url)
-    await page.wait_for_url("https://photos.google.com/share/*")
-    galbum_id, gkey = re.findall(r"share\/([^\/?]+).*[?&]key=([^\/&]+)", page.url)[0]
-    title = (await page.title()).replace(" - Google Photos", "")
-    return galbum_id, gkey, title
-
-
-async def download_photo(page: Page, album_id: str, key: str, image_id: str) -> tuple[Path, str]:
-    await page.goto(f"https://photos.google.com/share/{album_id}/photo/{image_id}?key={key}")
-    await asyncio.sleep(0.3)
-    async with page.expect_download() as download_info:
-        await page.keyboard.press("Shift+D")
-    download = await download_info.value
-    return await download.path(), download.suggested_filename
+@asynccontextmanager
+async def get_google_photos_api() -> AsyncGenerator[GooglePhotosApi]:
+    async with async_playwright() as playwright:
+        browser = await playwright.chromium.launch_persistent_context(
+            USER_DATA_FOLDER,
+            headless=HEADLESS,
+        )
+        page = await browser.new_page()
+        yield GooglePhotosApi(browser, page)
--- main.py
+++ main.py
@@ -1,44 +1,38 @@
 import asyncio
 
-from playwright.async_api import Page, async_playwright
 from tqdm import tqdm
 
-import google_photos as gphoto
 import immich
-from config import ALBUMS_FILE, HEADLESS, USER_DATA_FOLDER
+from config import ALBUMS_FILE
 from db import Album, Database, get_db
+from google_photos import GooglePhotosApi, get_google_photos_api
 
 
-async def google_to_immich(page: Page, album: Album, google_id: str, db: Database) -> None:
-    file, filename = await gphoto.download_photo(page, album.google_id, album.google_key, google_id)
+async def google_to_immich(album: Album, google_id: str, db: Database, gphoto: GooglePhotosApi) -> None:
+    file, filename = await gphoto.download_photo(album.google_id, album.google_key, google_id)
     immich_id = await immich.upload_photo(file, filename)
     await immich.add_photo_to_album(album.immich_id, immich_id)
     await db.create_photo(album.id, google_id, immich_id, "to_immich")
 
 
 async def main() -> None:
-    async with async_playwright() as playwright, get_db() as db:
-        browser = await playwright.chromium.launch_persistent_context(
-            USER_DATA_FOLDER,
-            headless=HEADLESS,
-        )
-        page = await browser.new_page()
+    async with get_db() as db, get_google_photos_api() as gphoto:
         for url in ALBUMS_FILE.read_text().splitlines():
-            album_id, key, title = await gphoto.load_album_meta(page, url)
+            album_id, key, title = await gphoto.load_album_meta(url)
             print(f"checcking album '{title}'")
             album = await db.get_or_create_album(album_id, key)
             if album.immich_id is None:
                 album.immich_id = await immich.create_album(title)
 
             photos = await db.get_photos(album.id)
-            google_photo_ids = set(await gphoto.get_photo_ids(page, album.google_id, album.google_key))
+            google_photo_ids = set(await gphoto.get_photo_ids(album.google_id, album.google_key))
             immich_photo_ids = set(await immich.get_photo_ids(album.immich_id))
 
             new_on_google_photos = google_photo_ids - {p.google_id for p in photos}
             if new_on_google_photos:
                 print("uploading images to Immich")
                 for google_id in tqdm(new_on_google_photos):
-                    await google_to_immich(page, album, google_id, db)
+                    await google_to_immich(album, google_id, db, gphoto)
 
             deleted_on_google_photos = {p.google_id for p in photos} - google_photo_ids
             if deleted_on_google_photos:
